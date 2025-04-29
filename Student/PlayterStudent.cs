467229512c49b61c7a3d49c225ceb9f15d19504f
toepoker <andcedmu@gmail.com>
2025-04-29 09:40:03 -0500

    fixed torque

--- Student/PlayterStudent.cs
+++ Student/PlayterStudent.cs
@@ -68,8 +68,11 @@ public partial class PlayterSim : Simulator
         Vex coupR = mA * Vex.Cross(rFR, Vex.Cross(Sz, rFRS));
 
         /* ---------------- Arm torques ---------------- */
-        double TL = -Iarm*(k*thetaL + c*omegaFL);
-        double TR = -Iarm*(k*thetaR + c*omegaFR);
+  
+        double Iarm = mA * L * L;                 // nondimensional Iarm
+        double TL   = -Iarm * (k*thetaL + c*omegaFL);
+        double TR   = -Iarm * (k*thetaR + c*omegaFR);
+
 
         /* ---------------- Clear A, B ----------------- */
         Array.Clear(A,0,A.Length); Array.Clear(B,0,B.Length);
