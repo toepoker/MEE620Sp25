ca861fb6a9f2d63b5857b2f03a06572fe2474354
toepoker <andcedmu@gmail.com>
2025-04-29 22:23:57 -0500

    test

--- Student/PlayterStudent.cs
+++ Student/PlayterStudent.cs
@@ -269,14 +269,14 @@ sys.SetB(7, 0.0);  // Q_vz
 sys.SolveGauss();
 
 
-    ff[0] = sys.Sol(0);
-    ff[1] = sys.Sol(1);
-    ff[2] = sys.Sol(2);
-    ff[3] = sys.Sol(3);
-    ff[4] = sys.Sol(4);
-    ff[5] = sys.Sol(5);
-    ff[6] = sys.Sol(6);
-    ff[7] = sys.Sol(7);
+    //ff[0] = sys.Sol(0);
+   // ff[1] = sys.Sol(1);
+   // ff[2] = sys.Sol(2);
+   // ff[3] = sys.Sol(3);
+   // ff[4] = sys.Sol(4);
+   // ff[5] = sys.Sol(5);
+   // ff[6] = sys.Sol(6);
+   // ff[7] = sys.Sol(7);
   
 
 
