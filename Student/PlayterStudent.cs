209e563cdec64cf93feec8d6ef6d78621d4d9e8d
toepoker <andcedmu@gmail.com>
2025-04-30 15:03:38 -0500

    t

--- Student/PlayterStudent.cs
+++ Student/PlayterStudent.cs
@@ -263,14 +263,6 @@ sys.SetB(7, 0.0);  // Q_vz
 // 4) Solve the 8×8 system
 sys.SolveGauss();
 
-ff[0]  = sys.Sol(0);   // dωx/dt
-ff[1]  = sys.Sol(1);   // dωy/dt
-ff[2]  = sys.Sol(2);   // dωx/dt
-ff[3]  = sys.Sol(3);   // dωy/dt
-ff[4]  = sys.Sol(4);   // dωx/dt
-ff[5]  = sys.Sol(5);   // dωy/dt
-ff[6]  = sys.Sol(6);   // dωx/dt
-ff[7]  = sys.Sol(7);   // dωy/dt
 
 
 
