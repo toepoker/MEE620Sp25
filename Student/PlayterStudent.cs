ccb18655298ceae3540ef6b05908785b38a72381
toepoker <andcedmu@gmail.com>
2025-05-02 19:25:24 -0500

    test

--- Student/PlayterStudent.cs
+++ Student/PlayterStudent.cs
@@ -141,9 +141,7 @@ Vex bZ = new Vex(2*(q1*q3 + q0*q2),
                  q0*q0 - q1*q1 - q2*q2 + q3*q3);
 
 // 2) Hinge‐axis unit in B‐frame
-Vex sZ = new Vex(0.0, -sinPhi, cosPhi);
-Vex rSL_G = bX + h * bY;
-Vex rSR_G = h * bY - bX;
+Vex sZ = new Vex(0.0, -sinPhi*by, cosPhi*bZ);
 
 Vex rFL_SL = L * (Math.Cos(thetaL) * bX +
                   Math.Sin(thetaL) * cosPhi * bY +
@@ -154,6 +152,8 @@ Vex rFR_SR = -L * (Math.Cos(thetaR) * bX +
                    Math.Sin(thetaR) * sinPhi * bZ);
 
 
+Vex rSL_G   = new Vex(bX + h * bY);
+Vex rSR_G   = new Vex(bY - h * bX );
 // 3) Shoulder & arm position vectors (B‐frame)
 //Vex rSL_G   = new Vex( 1.0, h, 0.0 );
 //Vex rSR_G   = new Vex(-1.0, h, 0.0 );
