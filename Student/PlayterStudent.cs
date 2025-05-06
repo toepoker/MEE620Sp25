633d46be5d5277afd6ea5892be0919237177ffbc
toepoker <andcedmu@gmail.com>
2025-05-06 14:27:31 -0500

    g

--- Student/PlayterStudent.cs
+++ Student/PlayterStudent.cs
@@ -258,7 +258,7 @@ Vex omegaCrossH = Vex.Cross( new Vex(omegaX,omegaY,omegaZ),
 // Rigid Body Inertia Terms
 // -----------------------------
 
-double A00 = rho2;
+double A00 = rho2 * bX.x;
 double A01 = rho2 * gammaY;
 double A02 = rho2 * gammaZ;
 
