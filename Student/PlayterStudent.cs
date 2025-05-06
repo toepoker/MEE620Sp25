cf2619e1e839d3e0787084fc3a78be52754da48d
toepoker <andcedmu@gmail.com>
2025-05-06 17:11:49 -0500

    t

--- Student/PlayterStudent.cs
+++ Student/PlayterStudent.cs
@@ -189,14 +189,14 @@ Vex vFL_wx = Vex.Cross(bX, rFL_G);
 Vex vFL_wy = Vex.Cross(bY, rFL_G);
 Vex vFL_wz = Vex.Cross(bZ, rFL_G);
 //Vex vFL_wL = Vex.Cross(sZ, rFL_SL);
-Vex vFL_wL = Vex.Cross(sZ, rFL_SL);
+Vex vFL_wL = Vex.Cross(-sZ, rFL_SL);
 
 //  Right arm:
 Vex vFR_wx = Vex.Cross(bX, rFR_G);
 Vex vFR_wy = Vex.Cross(bY, rFR_G);
 Vex vFR_wz = Vex.Cross(bZ, rFR_G);
 //Vex vFR_wR = Vex.Cross(sZ, rFR_SR);
-Vex vFR_wR = Vex.Cross(sZ, rFR_SR);
+Vex vFR_wR = Vex.Cross(-sZ, rFR_SR);
 
 // 7) Left‐arm transport/Coriolis terms
 Vex term1   = Vex.Cross(omegaB, Vex.Cross(omegaB,   rSL_G));
