6b0212cb837f8f9258a76f037f6f2c362f92c32f
toepoker <andcedmu@gmail.com>
2025-05-06 19:36:01 -0500

    t

--- Student/PlayterStudent.cs
+++ Student/PlayterStudent.cs
@@ -490,9 +490,9 @@ ff[10] = .5*(q3*omegaX + q0*omegaY - q1*omegaZ);
 ff[11] = .5*(-q2*omegaX + q1*omegaY + q0*omegaZ);
 ff[12] = omegaFL;   // θ̇L = ωFL
 ff[13] = omegaFR;   // θ̇R = ωFR
-ff[14] = vx;   // ẋG = vx
-ff[15] = vy;   // ẏG = vy
-ff[16] = vz;   // żG = vz
+ff[14] = 0;   // ẋG = vx
+ff[15] = 0;   // ẏG = vy
+ff[16] = 0;   // żG = vz
 
 
 SetDebugVal(0, omegaX); 
